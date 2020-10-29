2ba08b8b005e3ae17afe3d04760353c56bed9921
PhaniPavanK <kphanipavan@gmail.com>
2020-10-29 21:06:19 +0530

    Added assisted BM, made graph more user friendly.

--- bm.py
+++ bm.py
@@ -37,6 +37,7 @@ imageDecomposer = {
     (0, 0, 0): ' '
 }
 print('Ready, Loaded.')
+cpuInfo = c.get_cpu_info()
 
 
 def main(Threads=1, calcs=1, loop=1, tp=None, showPlot=False, saveToFile=True):
@@ -51,7 +52,7 @@ def main(Threads=1, calcs=1, loop=1, tp=None, showPlot=False, saveToFile=True):
     else:
         print('Using {} Calculations'.format(tp))
     time.sleep(0.75)
-    print('Running {}B Calculations, thru {} loops'.format(calcs, loop))
+    print('Running {}M Calculations, thru {} loops'.format(calcs, loop))
     res = []
     for _ in range(loop):
         print('Loop {}: '.format(_ + 1), end='')
@@ -94,7 +95,7 @@ def main(Threads=1, calcs=1, loop=1, tp=None, showPlot=False, saveToFile=True):
         except:
             print('Loop interrupted..')
             return None
-    print('Benchmarking Done for {} loops of {}B {} Multiplications'.format(
+    print('Benchmarking Done for {} loops of {}M {} Multiplications'.format(
         _ + 1, calcs, tp))
     time.sleep(1)
     print('Max: {}'.format(max(res)))
@@ -105,11 +106,12 @@ def main(Threads=1, calcs=1, loop=1, tp=None, showPlot=False, saveToFile=True):
         load = {
             'Platform': platform.platform(),
             'Python Version': platform.python_version(),
-            'CPU Info': c.get_cpu_info()['brand_raw'],
+            'CPU Info': cpuInfo['brand_raw'],
+            'Thread Count': cpuInfo['count'],
             'Benchmark Config': {
                 'Calculation Type': tp,
                 'Threads Used': Threads,
-                'Number of Calculations in B': calcs,
+                'Number of Calculations in M': calcs,
                 'Number of loops': loop
             },
             'Result': {
@@ -124,6 +126,15 @@ def main(Threads=1, calcs=1, loop=1, tp=None, showPlot=False, saveToFile=True):
 
     if showPlot:
         plt.plot(res)
+        plt.plot([np.average(res)] * loop)
+        plt.plot([np.average(res) + np.std(res)] * loop)
+        plt.plot([np.average(res) - np.std(res)] * loop)
+        plt.xlabel('Loop')
+        plt.ylabel('Time (lower is Better)')
+        plt.legend(
+            ['Result', 'Average', 'Stddev Upper Limit', 'Stddev Lower Limit'])
+        plt.plot(res, 'x')
+        print('Close the Graph Window to continue.')
         plt.show()
 
 
@@ -133,7 +144,7 @@ def multiply(tp, calcs):
     else:
         mul = pi
     x = mul
-    for _ in range(int(calcs)):
+    for _ in range(int(calcs) * 1000000):
         x *= mul
         x /= mul
     del x
@@ -157,10 +168,9 @@ def encoder(resBook):
 
 def resultPrinter(dataDict):
     keys = list(dataDict.keys())
-    print(keys)
     if list(keys) != [
-            'Platform', 'Python Version', 'CPU Info', 'Benchmark Config',
-            'Result', 'Timestamp'
+            'Platform', 'Python Version', 'CPU Info', 'Thread Count',
+            'Benchmark Config', 'Result', 'Timestamp'
     ]:
         print(
             'Error with data, Data is not as Expected, Try rerunning Benchmarks.'
@@ -170,12 +180,13 @@ def resultPrinter(dataDict):
         print('Platform: ', dataDict['Platform'])
         print('Python Version: ', dataDict['Python Version'])
         print('CPU Info: ', dataDict['CPU Info'])
+        print('CPU Thread Count: ', dataDict['Thread Count'])
         print('Benchmark Config: ')
         print('\tCalculation Type: ',
               dataDict['Benchmark Config']['Calculation Type'])
         print('\tThreads Used: ', dataDict['Benchmark Config']['Threads Used'])
-        print('\tNumber os Calculations in B: ',
-              dataDict['Benchmark Config']['Number of Calculations in B'])
+        print('\tNumber os Calculations in M: ',
+              dataDict['Benchmark Config']['Number of Calculations in M'])
         print('\tNumber of loops:',
               dataDict['Benchmark Config']['Number of loops'])
         print('Result: ')
@@ -210,6 +221,8 @@ def imageCreater(data):
         mat.append(row)
     print(np.array(mat).shape)
     tim = time.localtime()
+    print('Saving to Result_{}_{}_{}_{}.png'.format(tim.tm_yday, tim.tm_hour,
+                                                    tim.tm_min, tim.tm_sec))
     cv2.imwrite(
         'Result_{}_{}_{}_{}.png'.format(tim.tm_yday, tim.tm_hour, tim.tm_min,
                                         tim.tm_sec), np.array(mat))
@@ -235,15 +248,17 @@ def pacer(num):
         return num % 10
 
 
-def cli():
-    while True:
+def bmMenu():
+    try:
         print('Enter your selection:')
-        for _ in menuList:
-            print(_)
+        print('1. Guided Parameter entry')
+        print('2. Advanced Parameter entry')
         while True:
             try:
                 sel = int(input('Selection: '))
-                if sel in [1, 2, 3]:
+                if sel == 0:
+                    return None
+                if sel in [1, 2]:
                     break
                 else:
                     print('Enter the corresponding number.')
@@ -251,16 +266,140 @@ def cli():
                 print("Enter the corresponding number, not text.")
                 continue
         if sel == 1:
-            main()
+            payload = []
+            while True:
+                try:
+                    tr = int(
+                        input('Enter the number of Threads to use({} max): '.
+                              format(cpuInfo['count'])))
+                    if tr == 0:
+                        return None
+                    if tr > cpuInfo['count']:
+                        print(
+                            'Your CPU has a maximum of {} Threads, Enter a number less than that...'
+                            .format(cpuInfo['count']))
+                    else:
+                        break
+                except:
+                    print('Enter a number less than or equal to {}, not text'.
+                          format(cpuInfo['count']))
+            payload.append(tr)
+            del tr
+            while True:
+                try:
+                    cl = int(
+                        input(
+                            'Enter the number of Calculations in Millions: '))
+                    if cl == 0:
+                        return None
+                    break
+                except:
+                    print('Enter a number, not text.')
+            payload.append(cl)
+            del cl
+            while True:
+                try:
+                    lp = int(input("Enter the number of loops: "))
+                    if lp == 0:
+                        return None
+                    break
+                except:
+                    print('Enter a number, not text.')
+            payload.append(lp)
+            while True:
+                try:
+                    print('Enter the type of Calculation to be done.')
+                    print('1. Integer')
+                    print('2. Floating point number')
+                    tp = int(input('Enter your selection: '))
+                    if tp == 0:
+                        return None
+                    if tp == 1:
+                        payload.append('int')
+                        break
+                    elif tp == 2:
+                        payload.append('float')
+                        break
+                    else:
+                        print('Enter 1 or 2.')
+                except:
+                    print('Enter a number, not text.')
+            while True:
+                try:
+                    print('You want to see the plot of benchmark times?')
+                    print('1. Yes\n2. No')
+                    tp = int(input('Enter your selection: '))
+                    if tp == 0:
+                        return None
+                    if tp == 1:
+                        payload.append(True)
+                        break
+                    elif tp == 2:
+                        payload.append(False)
+                        break
+                    else:
+                        print('Enter 1 or 2.')
+                except:
+                    print('Enter a number, not text.')
+            while True:
+                try:
+                    print(
+                        'You want save the result to a WallMark encoded file?')
+                    print('1. Yes\n2. No')
+                    tp = int(input('Enter your selection: '))
+                    if tp == 0:
+                        return None
+                    if tp == 1:
+                        payload.append(True)
+                        break
+                    elif tp == 2:
+                        payload.append(False)
+                        break
+                    else:
+                        print('Enter 1 or 2.')
+                except:
+                    print('Enter a number, not text.')
+            main(payload[0], payload[1], payload[2], payload[3], payload[4],
+                 payload[5])
+        elif sel == 2:
+            pass
+        else:
+            print('Program Corrupt!!!')
+            exit()
+    except:
+        print('Process Stopped.')
+        return None
+
+
+def cli():
+    while True:
+        print('\nEnter your selection:')
+        for _ in menuList:
+            print(_)
+        while True:
+            try:
+                sel = int(input('Selection: '))
+                if sel in [1, 2, 3, 0]:
+                    break
+                else:
+                    print('Enter the corresponding number.')
+            except:
+                print("Enter the corresponding number, not text.")
+                continue
+        if sel == 1:
+            bmMenu()
         elif sel == 2:
             loc = input('Enter the absolute location of the Result file: ')
             imageDecoder(loc)
         elif sel == 3:
             exit()
+        elif sel == 0:
+            print('You are already on the Main Menu :|')
 
 
 if __name__ == '__main__':
-    print('Welcome to WallMark™️')
+    print('Welcome to WallMark™️\n')
+    print('*** Enter 0 as input to return to the Main Menu ***')
     cli()
     #main(Threads=2, calcs=10000000, loop=1, tp='int', saveToFile=True)
     #imageDecoder('result.png')
